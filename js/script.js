0df808ff89a97f3c0f9156fdf28a2735f692e91a
ihfernando <ihfernando@outlook.com>
2019-01-08 20:16:31 -0200

    Auto alteração de tema

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
-const swipe = document.getElementById('js--fe-swipe');
+const $swipe = document.getElementById('js--fe-swipe')
 
-swipe.addEventListener("click", () => {
-    if (swipe.classList.contains('js--fe-swipe__active')) {
-    swipe.classList.remove('js--fe-swipe__active')  
-    document.documentElement.style.setProperty('--color-first', '#222222');
-    document.documentElement.style.setProperty('--color-third', '#fbf9f7');
-    } else {
-    swipe.classList.add('js--fe-swipe__active')
-    document.documentElement.style.setProperty('--color-first', '#bebebe');
-    document.documentElement.style.setProperty('--color-third', '#222222');
+document.addEventListener('DOMContentLoaded', () => {
+    const isNight = new Date().getUTCHours() > 9 &&
+                    new Date().getUTCHours() < 22
+
+    if (!isNight) {
+        swipeCondition($swipe)
     }
-});
+})
+
+$swipe.addEventListener('click', () => {
+    swipeCondition($swipe)
+})
+
+const swipeCondition = $swipe => {
+    const getCheckbox = document.getElementById('js--fe-swipe__checkbox')
+
+    if ($swipe.classList.contains('js--fe-swipe__active')) {
+        $swipe.classList.remove('js--fe-swipe__active')
+        swipeMode(true)
+    } else {
+        $swipe.classList.add('js--fe-swipe__active')
+        swipeMode(false)
+        getCheckbox.setAttribute('checked','')
+    }
+}
+
+const swipeMode = isModeOn => {
+    if (isModeOn) {
+        document.documentElement.style.setProperty('--color-first', '#222222')
+        document.documentElement.style.setProperty('--color-third', '#fbf9f7')
+    } else {
+        document.documentElement.style.setProperty('--color-first', '#bebebe')
+        document.documentElement.style.setProperty('--color-third', '#222222')
+    }
+}
