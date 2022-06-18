409ad8d5d216d260753ae5a6cfc85f6a97d242ae
Fernando Gomes <ihfernando@outlook.com>
2022-06-18 00:54:03 -0300

    Arquivando antigo site, subindo atualização para novo modelo

--- js/script.js
+++ js/script.js
@@ -1,54 +1,75 @@
-const $swipe = document.getElementById('js--fe-swipe')
+/**
+ * Set current year on Footer
+ */
+const $year = document.getElementById("js--fe-set-current-year");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const isNight = new Date().getUTCHours() > 9 &&
-                    new Date().getUTCHours() < 22
+const setCurrentYearOnFooter = () => {
+  const currentYear = new Date().getFullYear();
 
-    if (!isNight) {
-        swipeCondition($swipe)
-    }
+  $year.innerHTML = currentYear;
+};
 
-    fadeEffect()
-})
+/**
+ * Swipe theme
+ */
+const $swipe = document.getElementById("js--fe-swipe");
 
-$swipe.addEventListener('click', () => {
-    swipeCondition($swipe)
-})
+$swipe.addEventListener("click", () => {
+  swipeCondition($swipe);
+});
 
-const swipeCondition = $swipe => {
-    const getCheckbox = document.getElementById('js--fe-swipe__checkbox')
+const swipeCondition = ($swipe) => {
+  const getCheckbox = document.getElementById("js--fe-swipe__checkbox");
 
-    if ($swipe.classList.contains('js--fe-swipe__active')) {
-        $swipe.classList.remove('js--fe-swipe__active')
-        swipeMode(true)
+  if ($swipe.classList.contains("js--fe-swipe__active")) {
+    $swipe.classList.remove("js--fe-swipe__active");
+    swipeMode(true);
+  } else {
+    $swipe.classList.add("js--fe-swipe__active");
+    swipeMode(false);
+    getCheckbox.setAttribute("checked", "");
+  }
+};
+
+const swipeMode = (isModeOn) => {
+  if (isModeOn) {
+    document.documentElement.style.setProperty("--color-first", "#222222");
+    document.documentElement.style.setProperty("--color-third", "#fbf9f7");
+  } else {
+    document.documentElement.style.setProperty("--color-first", "#bebebe");
+    document.documentElement.style.setProperty("--color-third", "#222222");
+  }
+};
+
+/**
+ * Set fadeEffect with 500ms to all fe-fade-in
+ */
+const fadeEffect = () => {
+  const $els = document.getElementsByClassName("fe-fade-in");
+
+  let count = 0;
+
+  setInterval(() => {
+    if (count === $els.length) {
+      clearInterval(this);
     } else {
-        $swipe.classList.add('js--fe-swipe__active')
-        swipeMode(false)
-        getCheckbox.setAttribute('checked','')
+      $els[count].classList.add("fe-fade-in__active");
+      count += 1;
     }
-}
+  }, 500);
+};
 
-const swipeMode = isModeOn => {
-    if (isModeOn) {
-        document.documentElement.style.setProperty('--color-first', '#222222')
-        document.documentElement.style.setProperty('--color-third', '#fbf9f7')
-    } else {
-        document.documentElement.style.setProperty('--color-first', '#bebebe')
-        document.documentElement.style.setProperty('--color-third', '#222222')
-    }
-}
+/**
+ * Dispatch Event on DOM Loaded
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  setCurrentYearOnFooter();
 
-const fadeEffect = () => { 
-    const $els = document.getElementsByClassName('fe-fade-in')
+  const isNight = new Date().getUTCHours() > 9 && new Date().getUTCHours() < 22;
 
-    let count = 0
+  if (!isNight) {
+    swipeCondition($swipe);
+  }
 
-    setInterval(() => {
-        if (count === $els.length) {
-            clearInterval(this)
-        } else {
-            $els[count].classList.add('fe-fade-in__active')
-            count += 1
-        }
-    }, 500)
-}
+  fadeEffect();
+});
---
+++ versions/v.4/js/script.js
@@ -0,0 +1,54 @@
+const $swipe = document.getElementById('js--fe-swipe')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const isNight = new Date().getUTCHours() > 9 &&
+                    new Date().getUTCHours() < 22
+
+    if (!isNight) {
+        swipeCondition($swipe)
+    }
+
+    fadeEffect()
+})
+
+$swipe.addEventListener('click', () => {
+    swipeCondition($swipe)
+})
+
+const swipeCondition = $swipe => {
+    const getCheckbox = document.getElementById('js--fe-swipe__checkbox')
+
+    if ($swipe.classList.contains('js--fe-swipe__active')) {
+        $swipe.classList.remove('js--fe-swipe__active')
+        swipeMode(true)
+    } else {
+        $swipe.classList.add('js--fe-swipe__active')
+        swipeMode(false)
+        getCheckbox.setAttribute('checked','')
+    }
+}
+
+const swipeMode = isModeOn => {
+    if (isModeOn) {
+        document.documentElement.style.setProperty('--color-first', '#222222')
+        document.documentElement.style.setProperty('--color-third', '#fbf9f7')
+    } else {
+        document.documentElement.style.setProperty('--color-first', '#bebebe')
+        document.documentElement.style.setProperty('--color-third', '#222222')
+    }
+}
+
+const fadeEffect = () => { 
+    const $els = document.getElementsByClassName('fe-fade-in')
+
+    let count = 0
+
+    setInterval(() => {
+        if (count === $els.length) {
+            clearInterval(this)
+        } else {
+            $els[count].classList.add('fe-fade-in__active')
+            count += 1
+        }
+    }, 500)
+}
